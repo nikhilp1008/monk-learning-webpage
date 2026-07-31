d6647d41a78ebeb7c18d73f86ca42e003a8fdaa1
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 00:35:58 -0500

    Ch09 Sec5: deriving P=P0+rho*g*h from a force balance

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -862,6 +862,7 @@ import Ch09Sec1 from "./Ch09Sec1";
 import Ch09Sec2 from "./Ch09Sec2";
 import Ch09Sec3 from "./Ch09Sec3";
 import Ch09Sec4 from "./Ch09Sec4";
+import Ch09Sec5 from "./Ch09Sec5";
 
 const CH09 = "33795397-f8fe-5ef6-ba2d-64549905ecd3"; // Class 11 · Mechanical Properties of Fluids
 
@@ -869,4 +870,5 @@ REGISTRY[`${CH09}:1`] = Ch09Sec1;
 REGISTRY[`${CH09}:2`] = Ch09Sec2;
 REGISTRY[`${CH09}:3`] = Ch09Sec3;
 REGISTRY[`${CH09}:4`] = Ch09Sec4;
+REGISTRY[`${CH09}:5`] = Ch09Sec5;
 /* ============================ end Ch09 ============================ */
