ef856f6e809a116d4a7e17f882f462e0b367ad2d
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 20:22:47 -0500

    Ch12 Sec13: gas laws formula sheet (Boyle/Charles/Gay-Lussac/Avogadro, PV=nRT, Dalton/Graham, constants)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -872,6 +872,7 @@ import Ch12Sec9 from "./Ch12Sec9";
 import Ch12Sec10 from "./Ch12Sec10";
 import Ch12Sec11 from "./Ch12Sec11";
 import Ch12Sec12 from "./Ch12Sec12";
+import Ch12Sec13 from "./Ch12Sec13";
 
 const CH12 = "8300dbf9-d9f7-505b-82c6-ad8d236eaff1"; // Class 11 · Kinetic Theory
 
@@ -887,4 +888,5 @@ REGISTRY[`${CH12}:9`] = Ch12Sec9;
 REGISTRY[`${CH12}:10`] = Ch12Sec10;
 REGISTRY[`${CH12}:11`] = Ch12Sec11;
 REGISTRY[`${CH12}:12`] = Ch12Sec12;
+REGISTRY[`${CH12}:13`] = Ch12Sec13;
 /* ============================ end Ch12 ============================ */
