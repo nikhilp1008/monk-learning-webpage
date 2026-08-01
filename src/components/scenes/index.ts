b8ffe27e541eddedbd3d61f74cdd78d70cf87227
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 21:11:23 -0500

    Ch12 Sec29: Maxwell distribution curve (base/heated/heavier overlays, area-reading rule, O2/H2 anchors)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -888,6 +888,7 @@ import Ch12Sec25 from "./Ch12Sec25";
 import Ch12Sec26 from "./Ch12Sec26";
 import Ch12Sec27 from "./Ch12Sec27";
 import Ch12Sec28 from "./Ch12Sec28";
+import Ch12Sec29 from "./Ch12Sec29";
 
 const CH12 = "8300dbf9-d9f7-505b-82c6-ad8d236eaff1"; // Class 11 · Kinetic Theory
 
@@ -919,4 +920,5 @@ REGISTRY[`${CH12}:25`] = Ch12Sec25;
 REGISTRY[`${CH12}:26`] = Ch12Sec26;
 REGISTRY[`${CH12}:27`] = Ch12Sec27;
 REGISTRY[`${CH12}:28`] = Ch12Sec28;
+REGISTRY[`${CH12}:29`] = Ch12Sec29;
 /* ============================ end Ch12 ============================ */
