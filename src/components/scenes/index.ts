490a64a1e86d159cc6b455db11ea0a88b31927c6
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 20:04:28 -0500

    Ch09 Sec56: power dissipated goes as r^5 worked example (drafted, unverified)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -913,6 +913,7 @@ import Ch09Sec52 from "./Ch09Sec52";
 import Ch09Sec53 from "./Ch09Sec53";
 import Ch09Sec54 from "./Ch09Sec54";
 import Ch09Sec55 from "./Ch09Sec55";
+import Ch09Sec56 from "./Ch09Sec56";
 
 const CH09 = "33795397-f8fe-5ef6-ba2d-64549905ecd3"; // Class 11 · Mechanical Properties of Fluids
 
@@ -971,4 +972,5 @@ REGISTRY[`${CH09}:52`] = Ch09Sec52;
 REGISTRY[`${CH09}:53`] = Ch09Sec53;
 REGISTRY[`${CH09}:54`] = Ch09Sec54;
 REGISTRY[`${CH09}:55`] = Ch09Sec55;
+REGISTRY[`${CH09}:56`] = Ch09Sec56;
 /* ============================ end Ch09 ============================ */
