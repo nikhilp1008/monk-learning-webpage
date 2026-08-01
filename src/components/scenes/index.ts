b4cb4fb7aac0c8f6e6d0b26518c5c0d110cedc2c
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 20:08:22 -0500

    Ch09 Sec59: why radius to the fourth (drafted, unverified)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -916,6 +916,7 @@ import Ch09Sec55 from "./Ch09Sec55";
 import Ch09Sec56 from "./Ch09Sec56";
 import Ch09Sec57 from "./Ch09Sec57";
 import Ch09Sec58 from "./Ch09Sec58";
+import Ch09Sec59 from "./Ch09Sec59";
 
 const CH09 = "33795397-f8fe-5ef6-ba2d-64549905ecd3"; // Class 11 · Mechanical Properties of Fluids
 
@@ -977,4 +978,5 @@ REGISTRY[`${CH09}:55`] = Ch09Sec55;
 REGISTRY[`${CH09}:56`] = Ch09Sec56;
 REGISTRY[`${CH09}:57`] = Ch09Sec57;
 REGISTRY[`${CH09}:58`] = Ch09Sec58;
+REGISTRY[`${CH09}:59`] = Ch09Sec59;
 /* ============================ end Ch09 ============================ */
