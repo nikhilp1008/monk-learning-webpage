cbb6b33a6d3f4b05664f02cc44c4e90bbbbd5e42
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 20:38:35 -0500

    Ch09 Sec81: coalescing droplets heat up worked example (drafted, unverified)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -938,6 +938,7 @@ import Ch09Sec77 from "./Ch09Sec77";
 import Ch09Sec78 from "./Ch09Sec78";
 import Ch09Sec79 from "./Ch09Sec79";
 import Ch09Sec80 from "./Ch09Sec80";
+import Ch09Sec81 from "./Ch09Sec81";
 
 const CH09 = "33795397-f8fe-5ef6-ba2d-64549905ecd3"; // Class 11 · Mechanical Properties of Fluids
 
@@ -1021,4 +1022,5 @@ REGISTRY[`${CH09}:77`] = Ch09Sec77;
 REGISTRY[`${CH09}:78`] = Ch09Sec78;
 REGISTRY[`${CH09}:79`] = Ch09Sec79;
 REGISTRY[`${CH09}:80`] = Ch09Sec80;
+REGISTRY[`${CH09}:81`] = Ch09Sec81;
 /* ============================ end Ch09 ============================ */
