64778fd52f44f163c4b3c720ad1b69ddca3d2b73
Nikhil <nikhilshetty392@gmail.com>
2026-07-31 20:47:46 -0500

    Ch09 Sec86: chapter cheat sheet (drafted, unverified)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -943,6 +943,7 @@ import Ch09Sec82 from "./Ch09Sec82";
 import Ch09Sec83 from "./Ch09Sec83";
 import Ch09Sec84 from "./Ch09Sec84";
 import Ch09Sec85 from "./Ch09Sec85";
+import Ch09Sec86 from "./Ch09Sec86";
 
 const CH09 = "33795397-f8fe-5ef6-ba2d-64549905ecd3"; // Class 11 · Mechanical Properties of Fluids
 
@@ -1031,4 +1032,5 @@ REGISTRY[`${CH09}:82`] = Ch09Sec82;
 REGISTRY[`${CH09}:83`] = Ch09Sec83;
 REGISTRY[`${CH09}:84`] = Ch09Sec84;
 REGISTRY[`${CH09}:85`] = Ch09Sec85;
+REGISTRY[`${CH09}:86`] = Ch09Sec86;
 /* ============================ end Ch09 ============================ */
