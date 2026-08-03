01fba7ca8f4af6e81e17195c08ab93ab3f60586c
Nikhil <nikhilshetty392@gmail.com>
2026-08-03 12:56:47 -0500

    Ch14 Sec4: wave toolkit (master relation, angular bridges, wave equation, sign trap, v vs particle velocity)

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -863,10 +863,12 @@ REGISTRY[`${CH07}:29`] = Ch07Sec29;
 import Ch14Sec1 from "./Ch14Sec1";
 import Ch14Sec2 from "./Ch14Sec2";
 import Ch14Sec3 from "./Ch14Sec3";
+import Ch14Sec4 from "./Ch14Sec4";
 
 const CH14 = "7dca7b5a-e77c-530d-bbe8-01a3518dc5d0"; // Class 11 · Waves
 
 REGISTRY[`${CH14}:1`] = Ch14Sec1;
 REGISTRY[`${CH14}:2`] = Ch14Sec2;
 REGISTRY[`${CH14}:3`] = Ch14Sec3;
+REGISTRY[`${CH14}:4`] = Ch14Sec4;
 /* ============================ end Ch14 ============================ */
