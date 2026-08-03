4cfc97a2643f9b0ec87f2e42856998a903a145a7
Nikhil <nikhilshetty392@gmail.com>
2026-08-03 14:43:50 -0500

    Ch14 Sec30: board derivation - the two Doppler cases

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -889,6 +889,7 @@ import Ch14Sec26 from "./Ch14Sec26";
 import Ch14Sec27 from "./Ch14Sec27";
 import Ch14Sec28 from "./Ch14Sec28";
 import Ch14Sec29 from "./Ch14Sec29";
+import Ch14Sec30 from "./Ch14Sec30";
 
 const CH14 = "7dca7b5a-e77c-530d-bbe8-01a3518dc5d0"; // Class 11 · Waves
 
@@ -921,4 +922,5 @@ REGISTRY[`${CH14}:26`] = Ch14Sec26;
 REGISTRY[`${CH14}:27`] = Ch14Sec27;
 REGISTRY[`${CH14}:28`] = Ch14Sec28;
 REGISTRY[`${CH14}:29`] = Ch14Sec29;
+REGISTRY[`${CH14}:30`] = Ch14Sec30;
 /* ============================ end Ch14 ============================ */
