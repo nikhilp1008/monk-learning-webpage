ecb9a7122c20f8e9d7bc4914c82f4a5c39620de6
Nikhil <nikhilshetty392@gmail.com>
2026-08-04 10:37:46 -0500

    C11 Chemistry Ch05 Sec 5: first-law and expansion-work formula toolkit
    Accumulating formula board: master ΔU=q+w, irreversible w=-Pext.ΔV,
    reversible isothermal w=-nRT ln(V2/V1)=-2.303nRT log(V2/V1), red R-value
    caution (8.314 not 0.0821), free-expansion w=0 closer. Verified both
    languages, VERDICT PASS.

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -2910,9 +2910,11 @@ import C11Ch05Sec1 from "./C11Ch05Sec1";
 import C11Ch05Sec2 from "./C11Ch05Sec2";
 import C11Ch05Sec3 from "./C11Ch05Sec3";
 import C11Ch05Sec4 from "./C11Ch05Sec4";
+import C11Ch05Sec5 from "./C11Ch05Sec5";
 
 const C11CH05 = "a6961d73-9ca9-5716-8e0c-61c69c5e343f";
 REGISTRY[`${C11CH05}:1`] = C11Ch05Sec1;
 REGISTRY[`${C11CH05}:2`] = C11Ch05Sec2;
 REGISTRY[`${C11CH05}:3`] = C11Ch05Sec3;
 REGISTRY[`${C11CH05}:4`] = C11Ch05Sec4;
+REGISTRY[`${C11CH05}:5`] = C11Ch05Sec5;
