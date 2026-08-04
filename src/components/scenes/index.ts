b40c7e6c47a588a70437bb972190b46d8c34cc7c
Nikhil <nikhilshetty392@gmail.com>
2026-08-04 12:08:05 -0500

    Ch02 Sec42: From orbit to orbital -- Schrodinger's probability cloud
    Opens subtopic 4 (Quantum Model, Orbitals & Electron Configurations).

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -2942,6 +2942,7 @@ import C11Ch02Sec38 from "./C11Ch02Sec38";
 import C11Ch02Sec39 from "./C11Ch02Sec39";
 import C11Ch02Sec40 from "./C11Ch02Sec40";
 import C11Ch02Sec41 from "./C11Ch02Sec41";
+import C11Ch02Sec42 from "./C11Ch02Sec42";
 
 const C11CH02 = "16bf043d-bc59-5ebb-93ad-7b0fddf484c9"; // Class 11 Chemistry · Structure of Atom
 
@@ -2976,3 +2977,4 @@ REGISTRY[`${C11CH02}:38`] = C11Ch02Sec38;
 REGISTRY[`${C11CH02}:39`] = C11Ch02Sec39;
 REGISTRY[`${C11CH02}:40`] = C11Ch02Sec40;
 REGISTRY[`${C11CH02}:41`] = C11Ch02Sec41;
+REGISTRY[`${C11CH02}:42`] = C11Ch02Sec42;
