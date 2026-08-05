f616ec7337af456f909a7833b1ce16fd29f0d59d
Nikhil <nikhilshetty392@gmail.com>
2026-08-04 23:04:38 -0500

    Ch02 Sec53: Worked example (JEE Main) -- 3d subshell and Fe2+

--- src/components/scenes/index.ts
+++ src/components/scenes/index.ts
@@ -2950,6 +2950,7 @@ import C11Ch02Sec46 from "./C11Ch02Sec46";
 import C11Ch02Sec47 from "./C11Ch02Sec47";
 import C11Ch02Sec48 from "./C11Ch02Sec48";
 import C11Ch02Sec52 from "./C11Ch02Sec52";
+import C11Ch02Sec53 from "./C11Ch02Sec53";
 
 const C11CH02 = "16bf043d-bc59-5ebb-93ad-7b0fddf484c9"; // Class 11 Chemistry · Structure of Atom
 
@@ -2992,3 +2993,4 @@ REGISTRY[`${C11CH02}:46`] = C11Ch02Sec46;
 REGISTRY[`${C11CH02}:47`] = C11Ch02Sec47;
 REGISTRY[`${C11CH02}:48`] = C11Ch02Sec48;
 REGISTRY[`${C11CH02}:52`] = C11Ch02Sec52;
+REGISTRY[`${C11CH02}:53`] = C11Ch02Sec53;
